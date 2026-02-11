3d3b83458ba3a6ee0ae216d4b310578f14e8cda8
technicolo <franfrare19@gmail.com>
2026-02-11 15:13:22 -0300

    cambios para poder deployar en vercel

---
+++ app/carrera/cliente-carrera.tsx
@@ -0,0 +1,42 @@
+"use client";
+export const dynamic = "force-dynamic";
+
+import { Td, Tr } from "@chakra-ui/react";
+import { useState } from "react";
+import GenericTable from "../../common/components/generic-table";
+import { CareerStudent } from "./interfaces/career-student.interface";
+
+const Carrera: React.FC = () => {
+  const [career, setCareer] = useState<CareerStudent[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  const TableHeader = ["Nombre Carrera", "Estado", "Año de ingreso"];
+
+  const renderCarrerRow = (careerStudent: CareerStudent) => {
+    return (
+      <Tr key={careerStudent.id}>
+        <Td>{careerStudent.Career.name}</Td>
+        <Td>{careerStudent.active ? "activo" : "inactivo"}</Td>
+        <Td>{new Date(careerStudent.student.yearEntry).getFullYear()}</Td>
+      </Tr>
+    );
+  };
+
+  return (
+    <>
+      {error && <p>{error}</p>}
+      {career ? (
+        <GenericTable
+          data={career}
+          TableHeader={TableHeader}
+          renderRow={renderCarrerRow}
+          caption="Carrera"
+        />
+      ) : (
+        <p>Loading....</p>
+      )}
+    </>
+  );
+};
+
+export default Carrera;
--- app/carrera/page.tsx
+++ app/carrera/page.tsx
@@ -1,42 +1,15 @@
-"use client";
+import { Suspense } from "react";
+import Carrera from './cliente-carrera';
+
+
+
+
 export const dynamic = "force-dynamic";
 
-import { Td, Tr } from "@chakra-ui/react";
-import { useState } from "react";
-import GenericTable from "../../common/components/generic-table";
-import { CareerStudent } from "./interfaces/career-student.interface";
-
-const Carrera: React.FC = () => {
-  const [career, setCareer] = useState<CareerStudent[]>([]);
-  const [error, setError] = useState<string | null>(null);
-
-  const TableHeader = ["Nombre Carrera", "Estado", "Año de ingreso"];
-
-  const renderCarrerRow = (careerStudent: CareerStudent) => {
-    return (
-      <Tr key={careerStudent.id}>
-        <Td>{careerStudent.Career.name}</Td>
-        <Td>{careerStudent.active ? "activo" : "inactivo"}</Td>
-        <Td>{new Date(careerStudent.student.yearEntry).getFullYear()}</Td>
-      </Tr>
-    );
-  };
-
+export default function Page() {
   return (
-    <>
-      {error && <p>{error}</p>}
-      {career ? (
-        <GenericTable
-          data={career}
-          TableHeader={TableHeader}
-          renderRow={renderCarrerRow}
-          caption="Carrera"
-        />
-      ) : (
-        <p>Loading....</p>
-      )}
-    </>
+    <Suspense fallback={null}>
+      <Carrera />
+    </Suspense>
   );
-};
-
-export default Carrera;
+}
